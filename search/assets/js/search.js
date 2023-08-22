3f1b8cfa4d8d5491117594db29296eca3985231e
Serhii <76422002+gitsolaicity@users.noreply.github.com>
2023-08-23 02:29:58 +0300

    Update search.js

--- search/assets/js/search.js
+++ search/assets/js/search.js
@@ -346,9 +346,9 @@ if (hasSearchWrapper) {
 
 
     // count total-result and time
-let totalResults = `<em>${filteredItemsLength}</em> {{ i18n "results" . }}`;
+let totalResults = `<em>${filteredItemsLength}</em> results`;
 let totalTime = ((endTime - startTime) / 1000).toFixed(3);
-totalTime = `- {{ i18n "in" . }} <em>${totalTime}</em> {{ i18n "seconds" . }}`;
+totalTime = `- in <em>${totalTime}</em> ${i18n("seconds")}`;
 
 searchResultInfo &&
   (searchResultInfo.innerHTML =
